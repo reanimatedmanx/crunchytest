3f34034b0b0fc19b6a18de7663a7a22a87a89ccd
reanimatedmanx <me@rmx.codes>
2023-10-07 01:27:54 +0300

    feat: add layout blockout

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,5 +1,18 @@
+import { Container, Box } from "@radix-ui/themes";
+
+// TODO Add import aliases
+import { Header } from "./shared/components/Header";
+import { Content } from "./shared/components/Content";
+import { MediaList } from "./features/Media/MediaList";
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <Container>
+      <Header></Header>
+      <Content>
+        <MediaList />
+      </Content>
+    </Container>
+  );
 }
 
 export default App;
