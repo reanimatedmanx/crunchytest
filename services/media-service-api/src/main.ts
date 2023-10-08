254d5695ecbe9f439e1a980c94d7bbcc2aaf6dd5
reanimatedmanx <me@rmx.codes>
2023-10-08 13:23:46 +0300

    chore (#29): add swagger for API docs

--- services/media-service-api/src/main.ts
+++ services/media-service-api/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as manifest from '@manifest';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // TODO Grab this from a `ConfigStore`
+  const ENV = process.env.NODE_ENV;
+
+  if (ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle(manifest.name)
+      .setVersion(manifest.version)
+      .setDescription(manifest.description)
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+  }
 
   // TODO Extract to envfile
   await app.listen(4200);
