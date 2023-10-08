c710bcaa8c5b21e9a43eefddf7274d4aa2c0b3c3
reanimatedmanx <me@rmx.codes>
2023-10-09 01:59:51 +0300

    feat (#13): add MediaItem to storybook

---
+++ apps/client/src/features/Media/MediaItem/MediaItem.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { MediaItem } from './MediaItem'
+
+const meta = {
+  title: 'Features/Media/MediaItem',
+  component: MediaItem,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof MediaItem>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
