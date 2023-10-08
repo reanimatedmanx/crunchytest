27ff72fcdc831cbd7e01b2e0603ced79c9d17fd4
reanimatedmanx <me@rmx.codes>
2023-10-08 23:12:29 +0300

    impr (#16): customize default theme

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,17 +1,23 @@
-import { Container, Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
-import "./theme.css";
+import { Container, Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
+import './theme.css';
 
 // TODO Add import aliases
-import { Header } from "./shared/components/Header";
-import { Content } from "./shared/components/Content";
-import { MediaList } from "./features/Media/MediaList";
+import { Header } from './shared/components/Header';
+import { Content } from './shared/components/Content';
+import { MediaList } from './features/Media/MediaList';
 function App() {
   // TODO: Remove me
   console.info(process.env.REACT_APP_PUBLIC_API);
 
   return (
-    <Theme appearance="dark">
+    <Theme
+      appearance="dark"
+      accentColor="blue"
+      grayColor="gray"
+      panelBackground="solid"
+      scaling="100%"
+    >
       <Container>
         <Header></Header>
         <Content>
