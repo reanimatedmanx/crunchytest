6a50ad293a152f54fb239622c83934ff509d3feb
reanimatedmanx <me@rmx.codes>
2023-10-09 02:00:36 +0300

    fix: update import from theme.css to App.css for SB

--- apps/client/.storybook/preview.ts
+++ apps/client/.storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react'
 import '@radix-ui/themes/styles.css'
-import '../src/theme.css'
+import '../src/App.css'
 
 const preview: Preview = {
   parameters: {
