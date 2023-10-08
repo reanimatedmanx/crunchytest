6d06de7b33672607f7a69bdb4cc96fb69e3d95e8
reanimatedmanx <me@rmx.codes>
2023-10-08 21:29:57 +0300

    chore (#30): add to UI env files the API addr

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,13 @@
-import { Container, Box } from "@radix-ui/themes";
+import { Container } from "@radix-ui/themes";
 
 // TODO Add import aliases
 import { Header } from "./shared/components/Header";
 import { Content } from "./shared/components/Content";
 import { MediaList } from "./features/Media/MediaList";
 function App() {
+  // TODO: Remove me
+  console.info(process.env.REACT_APP_PUBLIC_API);
+
   return (
     <Container>
       <Header></Header>
