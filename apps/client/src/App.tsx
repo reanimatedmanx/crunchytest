df0f8d84499c3bdd1aadfa145b41886cb168a868
reanimatedmanx <me@rmx.codes>
2023-10-09 00:00:21 +0300

    impr (#16): multiple global style improvements

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Container, Theme } from '@radix-ui/themes'
 import '@radix-ui/themes/styles.css'
-import './theme.css'
+import './App.css'
 
 // TODO Add import aliases
 import { Header } from './shared/components/Header'
@@ -8,14 +8,15 @@ import { Content } from './shared/components/Content'
 import { MediaList } from './features/Media/MediaList'
 import { AddMediaButton } from './features/Media/shared'
 import { Logo } from './shared/components/Logo'
+
 function App() {
   // TODO: Remove me
   console.info(process.env.REACT_APP_PUBLIC_API)
 
   return (
     <Theme
-      appearance="dark"
-      accentColor="blue"
+      appearance="inherit"
+      accentColor="orange"
       grayColor="gray"
       panelBackground="solid"
       scaling="100%"
