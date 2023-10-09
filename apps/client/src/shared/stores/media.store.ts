e6f7dcf23a6e2003ad088f737bdf40952733a6e6
reanimatedmanx <me@rmx.codes>
2023-10-09 08:49:45 +0300

    feat (#11): create barebones MediaStore

---
+++ apps/client/src/shared/stores/media.store.ts
@@ -0,0 +1,25 @@
+import { makeObservable } from 'mobx'
+
+export class MediaStore {
+  constructor() {
+    makeObservable(this)
+  }
+
+  // #region Observables
+
+  // ...
+
+  // #endregion
+
+  // #region Actions
+
+  // ...
+
+  // #endregion
+
+  // #region Computed fields
+
+  // ...
+
+  // #endregion
+}
