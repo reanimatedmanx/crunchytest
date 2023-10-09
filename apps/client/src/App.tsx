402e048179288c707604d918c57a63d9f79c7fcd
reanimatedmanx <me@rmx.codes>
2023-10-09 09:04:27 +0300

    fix: use MediaApplication in the root App component

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,19 +1,11 @@
-import { Container, Flex, Theme } from '@radix-ui/themes'
+import { Theme } from '@radix-ui/themes'
 import '@radix-ui/themes/styles.css'
 import './App.css'
 
 // TODO Add import aliases
-import { Header } from './shared/components/Header'
-import { Content } from './shared/components/Content'
-import { MediaList } from './features/Media/MediaList'
-import { Logo } from './shared/components/Logo'
-import { MediaFilterBar } from './features/Media/MediaFilterBar'
-import { MediaForm } from './features/Media/MediaForm'
+import { MediaApplication } from './features/Media/MediaApplication'
 
 function App() {
-  // TODO: Remove me
-  console.info(process.env.REACT_APP_PUBLIC_API)
-
   return (
     <Theme
       appearance="inherit"
@@ -22,20 +14,7 @@ function App() {
       panelBackground="solid"
       scaling="100%"
     >
-      <Container>
-        <Flex direction="column" gap="5" mt="5">
-          <Header>
-            <Logo />
-            <MediaForm />
-          </Header>
-          <Content>
-            <MediaFilterBar />
-            <Flex direction="column" mt="5">
-              <MediaList />
-            </Flex>
-          </Content>
-        </Flex>
-      </Container>
+      <MediaApplication />
     </Theme>
   )
 }
