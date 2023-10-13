e1d70cf36ddd6c6c52300e1eb1dfef2de9b57e84
reanimatedmanx <me@rmx.codes>
2023-10-13 03:05:50 +0300

    fix (#39): flaky initial loading

--- apps/client/src/features/Media/MediaList/MediaList.tsx
+++ apps/client/src/features/Media/MediaList/MediaList.tsx
@@ -1,16 +1,31 @@
+import { Flex } from '@radix-ui/themes'
 import { MediaItem } from '../MediaItem'
 import { List } from '../../../shared/components/List'
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import { AppStore } from '../../../shared/stores'
 import { observer } from 'mobx-react'
 import { NoData } from './NoData'
+import { AppState } from '../../../shared/enums'
+import { MediaApiState } from '../../../shared/enums/media-api-state.enum'
 
 export const MediaList: React.FC = observer(() => {
-  const store = useContext(AppStore).mediaStore
+  const store = useContext(AppStore)
+
+  const isReady = useMemo(
+    () =>
+      store.state === AppState.Ready &&
+      store.mediaStore.state === MediaApiState.Ready,
+    [store.state, store.mediaStore.state],
+  )
+
+  if (!isReady) {
+    // TODO: Add a nice animation or smth.
+    return <Flex></Flex>
+  }
 
   return (
     <List
-      items={store.mediaList.map((m) => (
+      items={store.mediaStore.mediaList.map((m) => (
         <MediaItem
           key={m.id}
           id={m.id}
